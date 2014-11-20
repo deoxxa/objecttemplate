43acb6189720478c814b5319ab477a89b2aef43c
deoxxa <deoxxa@fknsrs.biz>
2014-11-20 21:06:20 +1100

    apparently i wanted to use reflection in july and forgot to commit it

--- objecttemplate.go
+++ objecttemplate.go
@@ -1,57 +1,53 @@
 package objecttemplate
 
 import (
-	"fknsrs.biz/dotty"
+	"reflect"
+
+	"fknsrs.biz/p/dotty"
 )
 
 func Render(template interface{}, data interface{}) (res interface{}, err error) {
-	switch template.(type) {
-	case []interface{}:
-		tpl := template.([]interface{})
-		arr := make([]interface{}, len(template.([]interface{})))
-		res = arr
+	_type := reflect.TypeOf(template)
 
-		for i := range tpl {
-			if val, verr := Render(tpl[i], data); verr != nil {
-				err = verr
+	if _type == nil {
+		res = template
+	} else if _type.Kind() == reflect.String {
+		tpl := reflect.ValueOf(template).String()
 
+		if tpl[0:1] == "$" {
+			res, err = dotty.Get(data, tpl[1:])
+		} else {
+			res = tpl
+		}
+	} else if _type.Kind() == reflect.Map && _type.Key().Kind() == reflect.String {
+		tpl := reflect.ValueOf(template)
+		obj := map[string]interface{}{}
+
+		for _, k := range tpl.MapKeys() {
+			if val, _err := Render(tpl.MapIndex(k).Interface(), data); _err != nil {
+				err = _err
 				return
 			} else {
-				arr[i] = val
+				obj[k.String()] = val
 			}
 		}
-	case map[string]interface{}:
-		tpl := template.(map[string]interface{})
-		obj := map[string]interface{}{}
+
 		res = obj
+	} else if _type.Kind() == reflect.Array || _type.Kind() == reflect.Slice {
+		tpl := reflect.ValueOf(template)
+		arr := []interface{}{}
 
-		for k := range tpl {
-			switch tpl[k].(type) {
-			case map[string]interface{}:
-				if val, verr := Render(tpl[k], data); err != nil {
-					err = verr
-
-					return
-				} else {
-					obj[k] = val
-				}
-			case string:
-				if tpl[k].(string)[0:1] == "$" {
-					if val, verr := dotty.Get(data, tpl[k].(string)[1:]); verr != nil {
-						err = verr
-
-						return
-					} else {
-						obj[k] = val
-					}
-				} else {
-					obj[k] = tpl[k]
-				}
-			default:
-				obj[k] = tpl[k]
+		for i := 0; i < tpl.Len(); i++ {
+			if val, _err := Render(tpl.Index(i).Interface(), data); _err != nil {
+				err = _err
+				return
+			} else {
+				arr = append(arr, val)
 			}
 		}
-	default:
+
+		res = arr
+	} else {
 		res = template
 	}
 
